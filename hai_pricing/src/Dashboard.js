510298a2550fbe1ed6d89015a41c52108714cc64
Jake Mok <jakemok@berkeley.edu>
2023-11-20 12:39:17 -0800

    fixed graph updates

--- hai_pricing/src/Dashboard.js
+++ hai_pricing/src/Dashboard.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import  { ChartComponent, PriceControl, HintSection, SignalsSection } from './ChartComponents';
 import NavBar from './InterfaceComponents/navbar';
@@ -7,120 +7,160 @@ import {pushPrices, pushDemands, pushProfits, pushTimerForPrices, pushTimerForHi
 const Dashboard = ({sessionID}) => {
   const [p1Price, setP1Price] = useState(500); // assuming 500 is the default value for both sliders
   const [p2Price, setP2Price] = useState(500);
+  const [updateCounter, setUpdateCounter] = useState(0);
   const [profitData, setProfitData] = useState({
     labels: ['Y1 Q1', 'Y1 Q2', 'Y1 Q3', 'Y1 Q4', 'Y1 Q5', 'Y1 Q6', 'Y1 Q7'],
     datasets: [{
       label: 'Profit',
-      data: [1500000, 2000000, 2500000, 2700000, 3000000, 3200000, 3500000], // initial profit data
+      data: [], // initial profit data
       borderColor: 'teal',
       fill: false
     }]
   });
   const [pricesData, setPricesData] = useState({
-    labels: ['Y1 Q1', 'Y1 Q2', 'Y1 Q3', 'Y1 Q4', 'Y1 Q5', 'Y1 Q6', 'Y1 Q7'],
+    labels: ['Y1 Q1', 'Y1 Q2', 'Y1 Q3', 'Y1 Q4', 'Y1 Q5', 'Y1 Q6', 'Y1 Q7', 'Y1 Q8', 'Y1 Q9', 'Y1 Q10'],
     datasets: [
       {
         label: 'P1',
-        data: [100, 300, 400, 600, 750, 800, 850], // initial data
+        data: [], // initial data
         borderColor: 'blue',
         fill: false
       },
       {
         label: 'P2',
-        data: [150, 350, 450, 650, 700, 750, 900], // initial data
+        data: [], // initial data
         borderColor: 'purple',
         fill: false
       }
     ]
   });
   const [demandData, setDemandData] = useState({
-    labels: ['Y1 Q1', 'Y1 Q2', 'Y1 Q3', 'Y1 Q4', 'Y1 Q5', 'Y1 Q6', 'Y1 Q7'],
+    labels: ['Y1 Q1', 'Y1 Q2', 'Y1 Q3', 'Y1 Q4', 'Y1 Q5', 'Y1 Q6', 'Y1 Q7', 'Y1 Q8', 'Y1 Q9', 'Y1 10'],
     datasets: [
       {
         label: 'Demand P1',
-        data: [2800, 2700, 2600, 2550, 2500, 2450, 2400], // initial demand data for Product 1
+        data: [], // initial demand data for Product 1
         borderColor: 'green',
         fill: false
       },
       {
         label: 'Demand P2',
-        data: [2750, 2650, 2575, 2525, 2475, 2435, 2385], // initial demand data for Product 2
+        data: [], // initial demand data for Product 2
         borderColor: 'orange',
         fill: false
       }
     ]
   });  
+  function generateLabels(counter) {
+    const currentYear = Math.ceil(counter / 10); // Calculate current year
+    const quarters = ['Q1', 'Q2', 'Q3', 'Q4', 'Q5', 'Q6', 'Q7', 'Q8', 'Q9', 'Q10'];
+    return quarters.map(quarter => `Y${currentYear} ${quarter}`);
+  }
+  useEffect(() => {
+    if (updateCounter % 9 === 0 && updateCounter !== 0) {
+      // Generate new labels based on the current value of updateCounter
+      const newLabels = generateLabels(updateCounter);
+  
+      // Reset Prices Data with new labels and empty data
+      setPricesData({
+        labels: newLabels,
+        datasets: [
+          { label: 'P1', data: [], borderColor: 'blue', fill: false },
+          { label: 'P2', data: [], borderColor: 'purple', fill: false }
+        ]
+      });
+  
+      // Reset Profit Data with new labels and empty data
+      setProfitData({
+        labels: newLabels,
+        datasets: [{ label: 'Profit', data: [], borderColor: 'teal', fill: false }]
+      });
+  
+      // Reset Demand Data with new labels and empty data
+      setDemandData({
+        labels: newLabels,
+        datasets: [
+          { label: 'Demand P1', data: [], borderColor: 'green', fill: false },
+          { label: 'Demand P2', data: [], borderColor: 'orange', fill: false }
+        ]
+      });
+    }
+  }, [updateCounter]);
+  
   
   
   
   
   function handleSetPrice(newP1Price, newP2Price) {
+    // Update P1 and P2 prices
     setP1Price(newP1Price);
     setP2Price(newP2Price);
-    const newP1Data = pricesData.datasets[0].data.slice(); // create a copy of the current data
-    newP1Data.unshift(p1Price); // add the new price to the beginning
-    newP1Data.pop(); // remove the last price
-    
-    const newP2Data = pricesData.datasets[1].data.slice(); // create a copy of the current data
-    newP2Data.unshift(p2Price); // add the new price to the beginning
-    newP2Data.pop(); // remove the last price
-      
-    setPricesData(prevState => ({
-      ...prevState,
-      datasets: [
-        {
-          ...prevState.datasets[0],
-          data: newP1Data
-        },
-        {
-          ...prevState.datasets[1],
-          data: newP2Data
-        }
-      ]
-      
-    }));
-    const newProfitData = [...profitData.datasets[0].data];  // create a copy of the existing profit data
-    const dummyProfitValue = (p1Price + p2Price) * 1000;  // Dummy equation using the set prices
-    newProfitData.unshift(dummyProfitValue);  // add the new profit to the front
-      
-    setProfitData(prevState => ({
-      ...prevState,
-      datasets: [{
-        ...prevState.datasets[0],
-        data: newProfitData
-      }]
-    }));
+    const dummyProfitValue = (newP1Price + newP2Price) * 1000;  // Dummy calculation for profit
+    const dummyDemandValueP1 = Math.max(3000 - newP1Price, 1000); // Dummy demand equation for product 1
+    const dummyDemandValueP2 = Math.max(3000 - newP2Price, 1000); // Dummy demand equation for product 2
 
-    // For Product 1
-    const newDemandDataP1 = demandData.datasets[0].data.slice(0, -1);  // remove the last demand value
-    const dummyDemandValueP1 = Math.max(3000 - p1Price, 1000);  // Dummy demand equation for product 1
-    newDemandDataP1.unshift(dummyDemandValueP1);  // add the new demand to the front
-
-    // For Product 2
-    const newDemandDataP2 = demandData.datasets[1].data.slice(0, -1);  // remove the last demand value
-    const dummyDemandValueP2 = Math.max(3000 - p2Price, 1000);  // Dummy demand equation for product 2
-    newDemandDataP2.unshift(dummyDemandValueP2);  // add the new demand to the front
-
-    setDemandData(prevState => ({
-        ...prevState,
+  
+    // Increment the update counter and update the datasets
+    setUpdateCounter(prevCounter => {
+      const newCounter = prevCounter + 1;
+  
+      // Generate new labels based on the updated counter
+      const newLabels = generateLabels(newCounter);
+  
+      // Update Prices Data with new labels and new prices
+      setPricesData(prevData => ({
+        ...prevData,
+        labels: newLabels,
         datasets: [
-            {
-                ...prevState.datasets[0],
-                data: newDemandDataP1
-            },
-            {
-                ...prevState.datasets[1],
-                data: newDemandDataP2
-            }
+          {
+            ...prevData.datasets[0],
+            data: [...prevData.datasets[0].data, newP1Price].slice(-10) // append new P1 price
+          },
+          {
+            ...prevData.datasets[1],
+            data: [...prevData.datasets[1].data, newP2Price].slice(-10) // append new P2 price
+          }
         ]
-    }));
+      }));
+  
+      // Update Profit Data
+      setProfitData(prevProfitData => ({
+        ...prevProfitData,
+        labels: newLabels,
+        datasets: [{
+          ...prevProfitData.datasets[0],
+          data: [...prevProfitData.datasets[0].data, dummyProfitValue].slice(-10)
+        }]
+      }));
+  
+      // Update Demand Data
+      // Assuming similar logic for demand data updates
+      setDemandData(prevDemandData => ({
+        ...prevDemandData,
+        labels: newLabels,
+        datasets: [
+          {
+            ...prevDemandData.datasets[0],
+            data: [...prevDemandData.datasets[0].data, dummyDemandValueP1].slice(-10)
+          },
+          {
+            ...prevDemandData.datasets[1],
+            data: [...prevDemandData.datasets[1].data, dummyDemandValueP2].slice(-10)
+          }
+        ]
+      }));
+  
+      return newCounter; // Return the new counter value
+    });
+  
+    // Firebase-related calls
     pushPrices(sessionID, newP1Price, newP2Price);
-    pushTimerForPrices(sessionID)
-    //pushProfits(sessionID, newProfitData);
-    pushDemands(sessionID, dummyDemandValueP2, dummyDemandValueP2)
-
+    pushTimerForPrices(sessionID);
+    // Uncomment if you need to push the new profit and demand data to Firebase
+    pushProfits(sessionID, dummyProfitValue);
+    pushDemands(sessionID, dummyDemandValueP1, dummyDemandValueP2);
   }
-
+  
   function handleHint() {
     pushTimerForHints(sessionID)
     return
